1aef7d2ccdc547bd98e543850aad5b0fa21bf538
BryanL2303 <bryanlee0033@gmail.com>
2023-07-25 16:59:40 +0800

    Added seed to create Administrator account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+admin = Account.new(account_name: "Administrator", account_type: "Admin", password: "manager")
