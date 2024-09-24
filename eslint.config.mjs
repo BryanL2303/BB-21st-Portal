b68fc2259fb7f2184ddd865a7b5bcca627f93420
Bryan Lee <bryanlee0033@gmail.com>
2024-09-25 00:09:11 +0800

    Configure eslint settings
    Adjusting eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,16 +4,19 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: 'detect', // Automatically picks the version you have installed
+      },
+    },
+    env: {
+      node: true,
+      browser: true
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {settings: {
-    react: {
-      version: 'detect', // Automatically picks the version you have installed
-    },
-  }},
-  {env: {
-    node: true
-  }},
 ];
