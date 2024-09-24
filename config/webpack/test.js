255590ba2509e91107eccb4ee6e533b35482546b
Bryan Lee <bryanlee0033@gmail.com>
2024-09-24 18:01:20 +0800

    Test Unit Test
    Testing that unit test can run for components

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,15 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'test'
 
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+
+import { render, screen } from '@testing-library/react';
+import { LogInPage } from '../../app/javascript/packs/logInPage';
+
+test('renders correct text', () => {
+  render(<LogInPage />);
+  const element = screen.getByText(/expected text/i);
+  expect(element).toBeInTheDocument();
+});
