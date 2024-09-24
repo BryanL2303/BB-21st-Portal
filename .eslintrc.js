8715f84b34dc09a800873049bf6307fdf1d41aad
Bryan Lee <bryanlee0033@gmail.com>
2024-09-24 20:48:20 +0800

    Update eslint config
    Testing new configuration for eslint CI

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect', // Automatically picks the version you have installed
+    },
+  },
+  // Other ESLint configuration...
+};
---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {settings: {
+    react: {
+      version: 'detect', // Automatically picks the version you have installed
+    },
+  }},
+];
