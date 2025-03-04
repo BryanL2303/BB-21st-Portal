49651583e7d6db046f91c84e9fb732736df76303
Dylan <dylanyeowf@gmail.com>
2025-03-04 19:10:41 +0800

    Minor Changes to the Admin Page

--- app/javascript/packs/adminPage/AdminPage.jsx
+++ app/javascript/packs/adminPage/AdminPage.jsx
@@ -3,55 +3,54 @@ import axios from 'axios'
 import { handleServerError } from '../general/handleServerError';
 import { DatabaseTable } from './DatabaseTable';
 
-/*Only meant for admin to initialise the page
-*/
+// Only meant for admin to initialise the page
 const AdminPage = () => {
-  const [tableNames, setTableNames] = useState([]);
-  const [tableVisible, setTableVisible] = useState({});
+	const [tableNames, setTableNames] = useState([]);
+	const [tableVisible, setTableVisible] = useState({});
 
-  //If there is no ongoing session go back to log in page
-  axios.post("/application/0/check_session", {}, {
-    withCredentials: true
-  })
-  .then()
-  .catch(() => {window.location.href = '/'})
+	//If there is no ongoing session go back to log in page
+	axios.post("/application/0/check_session", {}, {
+		withCredentials: true
+	})
+	.then()
+	.catch(() => { window.location.href = '/' })
 
-  useEffect(() => {
-    axios.post('/api/admin/0/get_table_names', {}, {
-      withCredentials: true
-    })
-    .then(resp => {
-      setTableNames(resp.data)
-      let initialVisbility = {}
-      resp.data.map((table_name) => {
-        initialVisbility[table_name] = false
-      })
-      setTableVisible(initialVisbility)
-    })
-    .catch(resp => handleServerError(resp.response.status))
-  }, [])
+	useEffect(() => {
+		axios.post('/api/admin/0/get_table_names', {}, {
+			withCredentials: true
+		})
+		.then(resp => {
+			setTableNames(resp.data)
+			let initialVisbility = {}
+			resp.data.map((table_name) => {
+				initialVisbility[table_name] = false
+			})
+			setTableVisible(initialVisbility)
+		})
+		.catch(resp => handleServerError(resp.response.status))
+	}, [])
 
-  function toggleTableVisibility(table_name) {
-    let currentTable = {...tableVisible}
-    currentTable[table_name] = !currentTable[table_name]
-    setTableVisible(currentTable)
-  }
+	function toggleTableVisibility(table_name) {
+		let currentTable = { ...tableVisible }
+		currentTable[table_name] = !currentTable[table_name]
+		setTableVisible(currentTable)
+	}
 
-  return(
-    <div className='admin-page'>
-      <div className='page-container'>
-        {tableNames.map((table_name) => {
-          return(
-            <div key={table_name} className = "database-table">
-              <h1>{table_name}</h1>
-              <button onClick={() => {toggleTableVisibility(table_name)}}>Show Table</button>
-              {tableVisible[table_name] && <DatabaseTable table_name={table_name}/>}
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  )
+	return (
+		<div className='admin-page'>
+			<div className='page-container'>
+				{tableNames.map((table_name) => {
+					return (
+						<div key={table_name} className="database-table">
+							<h1>{table_name}</h1>
+							<button onClick={() => { toggleTableVisibility(table_name) }}>Show Table</button>
+							{tableVisible[table_name] && <DatabaseTable table_name={table_name} />}
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
 export { AdminPage }
