abd6884b02d2b00dc578337eb7a90a308329db8a
Dylan <dylanyeowf@gmail.com>
2025-03-09 21:47:56 +0800

    Removed unused import

--- app/javascript/packs/attendanceManagementPage/ParadeNoticePage.jsx
+++ app/javascript/packs/attendanceManagementPage/ParadeNoticePage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { handleServerError } from '../general/handleServerError'
 import { ParadeNoticePDF } from './ParadeNoticePDF'
 
 const ParadeNoticePage = () => {
