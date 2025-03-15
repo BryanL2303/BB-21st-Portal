eff78c9ac0b2d47bdd1a33941baca3ba9ad55ebf
Dylan <dylanyeowf@gmail.com>
2025-03-15 13:37:34 +0800

    Fixed undefined function

--- app/javascript/packs/general/Header.jsx
+++ app/javascript/packs/general/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { handleServerError } from './handleServerError'
 
 const Header = () => {
 	const [loggedIn, setLoggedIn] = useState(false)
