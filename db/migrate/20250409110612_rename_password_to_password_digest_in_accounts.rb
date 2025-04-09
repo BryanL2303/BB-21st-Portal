d4f213a7d76737ce4cda2b23dc3b41559bfe57b8
Dylan <dylanyeowf@gmail.com>
2025-04-09 21:53:14 +0800

    Fixed failed ROR test case

--- db/migrate/20250409110612_rename_password_to_password_digest_in_accounts.rb
+++ db/migrate/20250409110612_rename_password_to_password_digest_in_accounts.rb
@@ -1,5 +1,5 @@
 class RenamePasswordToPasswordDigestInAccounts < ActiveRecord::Migration[7.0]
   def change
-    rename_column :accounts, :password, :password_digest
+    rename_column :account, :password, :password_digest
   end
 end
