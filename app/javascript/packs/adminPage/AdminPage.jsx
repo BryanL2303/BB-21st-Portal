b8f71201a34ed2f626715cc8c0259c01d5b6d00c
Dylan <dylanyeowf@gmail.com>
2025-05-04 22:42:09 +0800

    Redesign the admin page

--- app/javascript/packs/adminPage/AdminPage.jsx
+++ app/javascript/packs/adminPage/AdminPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import { handleServerError } from '../general/handleServerError';
 import { DatabaseTable } from './DatabaseTable';
@@ -6,48 +6,63 @@ import { DatabaseTable } from './DatabaseTable';
 // Only meant for admin to initialise the page
 const AdminPage = () => {
 	const [tableNames, setTableNames] = useState([]);
-	const [tableVisible, setTableVisible] = useState({});
-
-	//If there is no ongoing session go back to log in page
-	axios.post("/application/0/check_session", {}, {
-		withCredentials: true
-	})
-	.then()
-	.catch(() => { window.location.href = '/' })
+	const [tableVisible, setTableVisible] = useState();
+	const tableRef = useRef(null);
+	const [isDragging, setIsDragging] = useState(false);
+	const [startX, setStartX] = useState(0);
+	const [scrollLeft, setScrollLeft] = useState(0);
 
 	useEffect(() => {
-		axios.post('/api/admin/0/get_table_names', {}, {
-			withCredentials: true
+		axios.post("/application/0/check_session", {}, { withCredentials: true })
+		.then(resp => {
+			if (resp.data.user?.account_type != 'Admin') window.location.href = '/'
 		})
+		.catch(err => handleServerError(err.response.status))
+
+		axios.post('/api/admin/0/get_table_names', {}, { withCredentials: true })
 		.then(resp => {
 			setTableNames(resp.data)
-			let initialVisbility = {}
-			resp.data.map((table_name) => {
-				initialVisbility[table_name] = false
-			})
-			setTableVisible(initialVisbility)
+			setTableVisible(resp.data[0])
 		})
-		.catch(resp => handleServerError(resp.response.status))
+		.catch(err => handleServerError(err.response.status))
 	}, [])
 
-	function toggleTableVisibility(table_name) {
-		let currentTable = { ...tableVisible }
-		currentTable[table_name] = !currentTable[table_name]
-		setTableVisible(currentTable)
-	}
+	const onMouseDown = (e) => {
+		setIsDragging(true);
+		setStartX(e.clientX - tableRef.current.offsetLeft);
+		setScrollLeft(tableRef.current.scrollLeft);
+	};
+
+	const onMouseLeave = () => {
+		setIsDragging(false);
+	};
+
+	const onMouseUp = () => {
+		setIsDragging(false);
+	};
+
+	const onMouseMove = (e) => {
+		if (!isDragging) return;
+		e.preventDefault();
+		const x = e.clientX - tableRef.current.offsetLeft;
+		const scroll = (startX - x) + scrollLeft;
+		tableRef.current.scrollLeft = scroll;
+	};
 
 	return (
 		<div className='admin-page'>
-			<div className='page-container'>
-				{tableNames.map((table_name) => {
-					return (
-						<div key={table_name} className="database-table">
-							<h1>{table_name}</h1>
-							<button onClick={() => { toggleTableVisibility(table_name) }}>Show Table</button>
-							{tableVisible[table_name] && <DatabaseTable table_name={table_name} />}
-						</div>
-					)
-				})}
+			<div className='admin-tables-list'>
+				<div>
+					{tableNames.map(tableName => (<React.Fragment key={tableName}>
+						<input type='radio' onChange={(e) => setTableVisible(e.target.id.split('-')[2])} id={`db-radio-${tableName}`} name='tables' />
+						<label htmlFor={`db-radio-${tableName}`}>
+							<p>{tableName}</p>
+						</label>
+					</React.Fragment>))}
+				</div>
+			</div>
+			<div className='page-container' ref={tableRef} onMouseDown={onMouseDown} onMouseLeave={onMouseLeave} onMouseUp={onMouseUp} onMouseMove={onMouseMove} style={{ cursor: isDragging ? 'grabbing' : 'grab' }}>
+				<DatabaseTable table_name={tableVisible} />
 			</div>
 		</div>
 	)
