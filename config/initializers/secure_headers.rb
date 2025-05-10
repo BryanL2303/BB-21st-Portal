b1f2713deff63c88ab88d31847653f30c2b9eaa3
Dylan <dylanyeowf@gmail.com>
2025-05-10 14:04:42 +0800

    Updated CSP

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -5,12 +5,12 @@ SecureHeaders::Configuration.default do |config|
     default_src: ["'self'"],
     script_src: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://site-assets.fontawesome.com'],
     object_src: ["'none'"],
-    style_src: ["'self'", 'https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css'],
+    style_src: ["'self'", "'unsafe-inline'", 'https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css'],
     font_src: ["'self'", 'https://site-assets.fontawesome.com'],
     img_src: ["'self'", 'data:'],
-    connect_src: ["'self'", 'data:'],
+    connect_src: ["'self'", 'data:', 'https://raw.githubusercontent.com'],
     media_src: ["'self'"],
-    frame_src: ["'none'"],
+    frame_src: ["'self'", 'blob:'],
     form_action: ["'self'"]
   }
 end
