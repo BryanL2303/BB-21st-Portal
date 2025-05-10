42f7330c5e49c7a59a251afcd4216dcf942950d6
Dylan <dylanyeowf@gmail.com>
2025-05-10 13:41:06 +0800

    updated CSP to whiteist data: for connect-src due to failure in rendering of parade notice PDF

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -3,12 +3,12 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: ["'self'"],
-    script_src: ["'self'", "'unsafe-inline'", 'https://site-assets.fontawesome.com'],
+    script_src: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://site-assets.fontawesome.com'],
     object_src: ["'none'"],
     style_src: ["'self'", 'https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css'],
     font_src: ["'self'", 'https://site-assets.fontawesome.com'],
     img_src: ["'self'", 'data:'],
-    connect_src: ["'self'"],
+    connect_src: ["'self'", 'data:'],
     media_src: ["'self'"],
     frame_src: ["'none'"],
     form_action: ["'self'"]
